2c1cd410a6ae924db07cc6dfab48e95c0aa6b863
Carlphilip Nyström <120023333+callephille@users.noreply.github.com>
2024-05-23 12:09:42 +0200

    ändrat lite färger i mui componenterna

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css';
+import React from 'react';
+import './App.css'
+import { Stack, Button } from '@mui/material';
 
 function App() {
-	return (
-		<div className="App">
-			<h1>Techover Self Made - Spotify</h1>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Techover Self Made - Spotify</h1>
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { ThemeProvider } from '@mui/system';
+import { themeOptions } from './theme/meterial-theme';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={themeOptions}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
+
