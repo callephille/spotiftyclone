ced3c669816c9f18db7381be4fc0e64e827694ce
Carlphilip Nyström <120023333+callephille@users.noreply.github.com>
2024-06-18 14:29:01 +0200

    nu funkar allt

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,41 @@
-import {Box, Button} from '@mui/material'
+import { Box, Button } from '@mui/material';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
 
-
-const Home = ( ) => {
-    return(
-        
+const Home = () => {
+    return (
         <Box sx={{display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        flex: 1,
-        flexDirection: 'column',
-        gap: 5
-           }}>
-
-            <img src="/DSC_2423.jpg" alt="alle"style={{ maxWidth:'50%', maxHeight:'50%'}} />
-            <Button href='tel:0727025645' variant='contained' size='large'>
-                kontakta mig
-            </Button>
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            flex: 1,
+            flexDirection: 'column',
+            gap: 5
+               }}>
+    
+            <img src="/DSC_2423.jpg" alt="alle" style={{ maxWidth: '50%', maxHeight: '50%' }} />
+            <Box>
+                <Button
+                    href='tel:0727025645'
+                    variant='contained'
+                    size='large'
+                    startIcon={<PhoneIcon />}
+                >
+                    Ring mig
+                </Button></Box>
+                <Box>
+                <Button
+                    href='mailto:carlphilip97@gmail.com'
+                    variant='contained'
+                    size='large'
+                    startIcon={<EmailIcon />}
+                    
+                >
+                    Skicka Mail
+                </Button>
+            </Box>
         </Box>
     );
 };
 
-export default Home
+export default Home;
